18132bd444fed6fe6a0e761c51d5f1a42af15ed6
BW <17097491+bw-01@users.noreply.github.com>
2024-10-04 16:25:09 +1300

    Created initial page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+class Project {
+  constructor(TodoItem) {
+    this.TodoItem = TodoItem;
+  }
+}
+
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
