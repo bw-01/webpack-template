543b72524d89ccbd29ca97929f16a77ff0b909f8
BW <17097491+bw-01@users.noreply.github.com>
2024-10-22 09:38:21 +1300

    Feat: added Jest support for testing

--- setup-project.ps1
+++ setup-project.ps1
@@ -1,2 +1,2 @@
-npm install --save-dev webpack webpack-cli html-webpack-plugin style-loader css-loader webpack-dev-server eslint gh-pages
+npm install --save-dev webpack webpack-cli html-webpack-plugin style-loader css-loader webpack-dev-server eslint gh-pages jest
 npm install -D babel-loader @babel/core @babel/preset-env webpack
